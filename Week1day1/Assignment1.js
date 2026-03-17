4cdc3864cdd2981aeeeed3f17029accac0943320
Sarjun C <143405226+Sarjun777@users.noreply.github.com>
2026-03-17 18:55:08 +0530

    Add files via upload
    Day 1

---
+++ Week1day1/Assignment1.js
@@ -0,0 +1,21 @@
+let empname="Sarjun";
+console.log(empname);
+console.log(typeof empname);
+
+let empid=12345
+console.log(empid);
+console.log(typeof empid);
+
+let isplaywright=true
+console.log(typeof isplaywright)
+
+let longnumber=213492304920329323n
+console.log(longnumber);
+console.log(typeof longnumber);
+
+let employee
+console.log(typeof employee)
+
+let nameis=null
+console.log(typeof nameis)
+
